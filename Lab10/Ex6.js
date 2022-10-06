261f778241d10d16ea1b27eb7f88383df687c937
etachino <etachino@hawaii.edu>
2022-10-06 12:54:09 -1000

    push

--- Lab10/Ex6.js
+++ Lab10/Ex6.js
@@ -17,7 +17,6 @@ function download(url) {
         // script to download the picture here
         console.log(`Downloading ${url} ...`);
         picture_data = "image data:XOXOXO";
-        function callback(picture_data)
     }, 3* 1000);
     return picture_data;
 }
@@ -26,3 +25,5 @@ function process(picture) {
     console.log(`Processing ${picture}`);
 }
 
+let url = 'https://www.example.comt/big_pic.jpg';
+process( download(url) );
--- Lab11/Ex1.js
+++ Lab11/Ex1.js
@@ -1,6 +1,12 @@
 step1=00;
 step2=parseInt(step1/4);
-step3 = step2 + step1; // answer 
-step4 = 21; // Not Jan, so look at month before on table
+step3 = step2 + step1; 
+step4 = 3; // Not Jan, so look at month before on table
 step6 = step4 + step3; // 
-step7 = step4 + step6 
+step7 = step6 + 21;
+// Step8 total 24
+step9 = step7 - 2; 
+if(step7 > 7) {
+    step9%7;
+    console.log(step9%7)
+}
