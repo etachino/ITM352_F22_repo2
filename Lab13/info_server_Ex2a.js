d3b30b49db7ed9b66d1569cb35c052a1103dd047
etachino <etachino@hawaii.edu>
2022-10-22 02:04:57 -1000

    push

---
+++ Lab13/info_server_Ex2a.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.get('/test', function (request, response, next) {
+    console.log('Got a test path');
+    next();
+});
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
+// app.all - will respond to wither get or post 
+// * - every path 
+// rule action with set of pairs - can put any as many rules 
+// server can respond to request 
