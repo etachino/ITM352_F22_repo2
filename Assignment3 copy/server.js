9f4f0117ff5cf854580f17f65d82f2cf10324621
etachino <etachino@hawaii.edu>
2022-12-13 04:46:51 -1000

    push

--- Assignment3 copy/server.js
+++ Assignment3 copy/server.js
@@ -66,7 +66,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-      maxAge: 50000, 
+      maxAge: 500000, 
       httpOnly: true,
       signed: true,
       secure: false
